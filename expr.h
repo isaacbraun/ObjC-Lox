062169a3efd04225bf3685ff045fb5018946d6a4
isaacbraun <isaacbraun2000@gmail.com>
2022-05-31 16:21:14 -0400

    Whitespacing Fix

--- expr.h
+++ expr.h
@@ -5,84 +5,70 @@
 + (void)accept:(id)visitor;
 @end
 
+
 @interface Assign : Expr {
     Token *name;
     Expr *value;
 }
-
 @property(nonatomic, readwrite, retain) Token *name;
 @property(nonatomic, readwrite, retain) Expr *value;
-
 - (instancetype)initWithName:(Token *)param_name value:(Expr *)param_value;
-
 @end
+
 
 @interface Binary : Expr {
     Expr *left;
     Token *operator;
     Expr *right;
 }
-
 @property(nonatomic, readwrite, retain) Expr *left;
 @property(nonatomic, readwrite, retain) Token *operator;
 @property(nonatomic, readwrite, retain) Expr *right;
-
 - (instancetype)initWithLeft:(Expr *)param_left operator:(Token *)param_operator right:(Expr *)param_right;
-
 @end
+
 
 @interface Grouping : Expr {
     Expr *expression;
 }
-
 @property(nonatomic, readwrite, retain) Expr *expression;
-
 - (instancetype)initWithExpression:(Expr *)param_expression;
-
 @end
+
 
 @interface Literal : Expr {
     id value;
 }
-
 @property(nonatomic, readwrite, retain) id value;
-
 - (instancetype)initWithValue:(id)param_value;
-
 @end
+
 
 @interface Logical : Expr {
     Expr *left;
     Token *operator;
     Expr *right;
 }
-
 @property(nonatomic, readwrite, retain) Expr *left;
 @property(nonatomic, readwrite, retain) Token *operator;
 @property(nonatomic, readwrite, retain) Expr *right;
-
 - (instancetype)initWithLeft:(Expr *)param_left operator:(Token *)param_operator right:(Expr *)param_right;
-
 @end
+
 
 @interface Unary : Expr {
     Token *operator;
     Expr *right;
 }
-
 @property(nonatomic, readwrite, retain) Token *operator;
 @property(nonatomic, readwrite, retain) Expr *right;
-
 - (instancetype)initWithOperator:(Token *)param_operator right:(Expr *)param_right;
-
 @end
+
 
 @interface Variable : Expr {
     Token *name;
 }
-
 @property(nonatomic, readwrite, retain) Token *name;
-
 - (instancetype)initWithName:(Token *)param_name;
-
 @end
